a043c968a4522075695dd934cd9c7fa97fe130a3
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-03 13:48:27 -0400

    Update MonthlyCap.js

--- server/models/MonthlyCap.js
+++ server/models/MonthlyCap.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const monthlyCapSchema = new mongoose.Schema({
+  userId: { // Added
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true, // One cap per user
+  },
   cap: {
     type: Number,
+    required: true,
   },
 });
 
