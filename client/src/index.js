550074610ba3a28e93dc5ac30fb39243c2a705eb
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-03 16:34:35 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// AuthProvider and BudgetsProvider are now managed within App.js
 
 ReactDOM.render(
   <React.StrictMode>
