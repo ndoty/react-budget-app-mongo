24db85868e7143b785bb02554a433812a078ec80
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-05 23:12:58 -0400

    Update BudgetCard.js

--- client/src/components/BudgetCard.js
+++ client/src/components/BudgetCard.js
@@ -23,11 +23,10 @@ export default function BudgetCard({
         <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
-            {currencyFormatter.format(amount)}
             {max && (
+              {getRemaining(amount, max)}
               <span className="text-muted fs-6 ms-1">
-                from {currencyFormatter.format(max)}
-                 = {getRemaining(amount, max)}
+                {currencyFormatter.format(max)}
               </span>
             )}
           </div>
