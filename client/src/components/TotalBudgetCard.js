ecd52451c4539f6aecb6efcc604c5224f9e687d1
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-10 17:40:35 -0400

    Update TotalBudgetCard.js

--- client/src/components/TotalBudgetCard.js
+++ client/src/components/TotalBudgetCard.js
@@ -2,11 +2,17 @@ import { useBudgets } from "../contexts/BudgetsContext"
 import BudgetCard from "./BudgetCard"
 
 export default function TotalBudgetCard() {
-  const { expenses, budgets, monthlyCap } = useBudgets()
+  const { expenses, monthlyCap } = useBudgets()
   const amount = expenses.reduce((total, expense) => total + expense.amount, 0)
 
-  const max = monthlyCap.length > 0 && monthlyCap[0].cap !== 0 ? monthlyCap[0].cap : budgets.reduce((total, budget) => total + budget.max, 0)
-  if (max === 0) return null
+  // MODIFIED: 'max' is now determined ONLY by the monthlyCap.
+  // If no valid monthly cap is set, max will be null.
+  const max = monthlyCap.length > 0 && monthlyCap[0].cap > 0 ? monthlyCap[0].cap : null
 
+  // Don't show the card at all if there's no spending.
+  if (amount === 0) return null
+
+  // The underlying BudgetCard component will automatically hide the progress bar
+  // and "Remaining" text when the 'max' prop is null.
   return <BudgetCard amount={amount} name="Total" gray max={max} hideButtons />
 }
