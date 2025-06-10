6aa55262835228703dc026fe42b878eff78dea2c
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-10 16:29:42 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,8 @@
 // client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // This line requires a default export from App.js
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Optional: Log to verify REACT_APP_API_URL is picked up
-console.log('----------------------------------------------------------');
-console.log('[Client Startup - index.js] Environment Variables:');
-console.log(`[Client Startup - index.js] process.env.REACT_APP_API_URL: ${process.env.REACT_APP_API_URL}`);
-console.log(`[Client Startup - index.js] process.env.NODE_ENV: ${process.env.NODE_ENV}`);
-console.log('----------------------------------------------------------');
 
 ReactDOM.render(
   <React.StrictMode>
