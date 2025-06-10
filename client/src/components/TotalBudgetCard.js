50e15afa8caa2de0ce0d0c1df6002b74842fc8ad
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-10 14:19:58 -0400

    Update TotalBudgetCard.js

--- client/src/components/TotalBudgetCard.js
+++ client/src/components/TotalBudgetCard.js
@@ -5,7 +5,7 @@ export default function TotalBudgetCard() {
   const { expenses, budgets, monthlyCap } = useBudgets()
   const amount = expenses.reduce((total, expense) => total + expense.amount, 0)
 
-  const max = monthlyCap.length > 0 && monthlyCap[0].cap != '0' ? monthlyCap[0].cap : budgets.reduce((total, budget) => total + budget.max, 0)
+  const max = monthlyCap.length > 0 && monthlyCap[0].cap !== 0 ? monthlyCap[0].cap : budgets.reduce((total, budget) => total + budget.max, 0)
   if (max === 0) return null
 
   return <BudgetCard amount={amount} name="Total" gray max={max} hideButtons />
