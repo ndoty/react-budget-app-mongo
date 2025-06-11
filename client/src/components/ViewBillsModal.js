b80b99014ec4242810a388cc9c11deaf6903a398
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-11 15:09:05 -0400

    Update ViewBillsModal.js

--- client/src/components/ViewBillsModal.js
+++ client/src/components/ViewBillsModal.js
@@ -3,10 +3,9 @@ import { useBudgets } from "../contexts/BudgetsContext";
 import { currencyFormatter } from "../utils";
 
 export default function ViewBillsModal({ show, handleClose, onEditExpenseClick }) {
-  const { expenses, getBudget, deleteExpense } = useBudgets();
+  const { getBillExpenses, deleteExpense } = useBudgets();
 
-  // Filter to get only the expenses that are bills
-  const billExpenses = expenses.filter(expense => expense.isBill);
+  const billExpenses = getBillExpenses();
 
   return (
     <Modal show={show} onHide={handleClose}>
@@ -15,29 +14,22 @@ export default function ViewBillsModal({ show, handleClose, onEditExpenseClick }
       </Modal.Header>
       <Modal.Body>
         <Stack direction="vertical" gap="3">
-          {billExpenses.map(expense => {
-            // Find the budget name for display, or show "Uncategorized"
-            const budget = getBudget(expense.budgetId);
-            return (
-              <Stack direction="horizontal" gap="2" key={expense.id}>
-                <div className="me-auto">
-                    <div className="fs-4">{expense.description}</div>
-                    <div className="text-muted fs-6">{budget?.name || 'Uncategorized'}</div>
-                </div>
-                <div className="fs-5">{currencyFormatter.format(expense.amount)}</div>
-                <Button onClick={() => onEditExpenseClick(expense.id)} size="sm" variant="outline-primary">
-                  Edit
-                </Button>
-                <Button
-                  onClick={() => deleteExpense({ id: expense.id })}
-                  size="sm"
-                  variant="outline-danger"
-                >
-                  &times;
-                </Button>
-              </Stack>
-            );
-          })}
+          {billExpenses.map(expense => (
+            <Stack direction="horizontal" gap="2" key={expense.id}>
+              <div className="me-auto fs-4">{expense.description}</div>
+              <div className="fs-5">{currencyFormatter.format(expense.amount)}</div>
+              <Button onClick={() => onEditExpenseClick(expense.id)} size="sm" variant="outline-primary">
+                Edit
+              </Button>
+              <Button
+                onClick={() => deleteExpense({ id: expense.id })}
+                size="sm"
+                variant="outline-danger"
+              >
+                &times;
+              </Button>
+            </Stack>
+          ))}
         </Stack>
       </Modal.Body>
     </Modal>
