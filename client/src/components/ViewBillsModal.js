b72aded0eeaebb32fab540472fa2b67226915ad6
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-11 16:08:13 -0400

    Update ViewBillsModal.js

--- client/src/components/ViewBillsModal.js
+++ client/src/components/ViewBillsModal.js
@@ -6,6 +6,13 @@ export default function ViewBillsModal({ show, handleClose, onEditExpenseClick }
   const { getBillExpenses, deleteExpense } = useBudgets();
 
   const billExpenses = getBillExpenses();
+  
+  // MODIFIED: Revert sorting logic to a simple numeric sort on the dueDate
+  const sortedBillExpenses = [...billExpenses].sort((a, b) => {
+    if (a.dueDate < b.dueDate) return -1;
+    if (a.dueDate > b.dueDate) return 1;
+    return a.description.localeCompare(b.description);
+  });
 
   return (
     <Modal show={show} onHide={handleClose}>
@@ -14,10 +21,15 @@ export default function ViewBillsModal({ show, handleClose, onEditExpenseClick }
       </Modal.Header>
       <Modal.Body>
         <Stack direction="vertical" gap="3">
-          {billExpenses.map(expense => (
+          {sortedBillExpenses.map(expense => (
             <Stack direction="horizontal" gap="2" key={expense.id}>
-              {/* MODIFIED: The budget name/uncategorized text is removed */}
-              <div className="me-auto fs-4">{expense.description}</div>
+              <div className="me-auto">
+                  <div className="fs-4">{expense.description}</div>
+                  {/* MODIFIED: Revert display to show the day of the month */}
+                  {expense.dueDate && (
+                    <div className="text-muted fs-6">Due on day: {expense.dueDate}</div>
+                  )}
+              </div>
               <div className="fs-5">{currencyFormatter.format(expense.amount)}</div>
               <Button onClick={() => onEditExpenseClick(expense.id)} size="sm" variant="outline-primary">
                 Edit
