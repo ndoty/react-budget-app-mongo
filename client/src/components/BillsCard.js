0ab8148dc47a0e74e6df0d6b558b3bf9bbc293b1
Nicholas Doty <n.faustdoty@gmail.com>
2025-06-11 13:29:29 -0400

    Update BillsCard.js

--- client/src/components/BillsCard.js
+++ client/src/components/BillsCard.js
@@ -1,34 +1,36 @@
 import { Button, Card, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
-import { useBudgets } from "../contexts/BudgetsContext";
+import { useBudgets, BILLS_BUDGET_ID } from "../contexts/BudgetsContext";
 
-export default function BillsCard({ onViewExpensesClick }) {
-  const { expenses } = useBudgets();
+export default function BillsCard(props) {
+  const { getBudgetExpenses } = useBudgets();
+  
+  // Get all expenses assigned to the "Bills" category
+  const billsExpenses = getBudgetExpenses(BILLS_BUDGET_ID);
+  const amount = billsExpenses.reduce(
+    (total, expense) => total + expense.amount,
+    0
+  );
 
-  // Filter all expenses to find only the ones marked as a bill
-  const billsTotal = expenses
-    .filter(expense => expense.isBill)
-    .reduce((total, expense) => total + expense.amount, 0);
-
-  // Don't show the card if there are no expenses marked as bills
-  if (billsTotal === 0) {
-    return null;
+  if (amount === 0) {
+    return null; // Don't show the card if there are no bills
   }
 
   return (
+    // Using a different color to distinguish from regular budgets
     <Card className="bg-info bg-opacity-10">
       <Card.Body>
         <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
-          <div className="me-2">Recurring Bills</div>
+          <div className="me-2">Bills</div>
           <div className="d-flex align-items-baseline">
-            {currencyFormatter.format(billsTotal)}
+            {currencyFormatter.format(amount)}
           </div>
         </Card.Title>
         <Stack direction="horizontal" gap="2" className="mt-4">
           <Button
             variant="outline-secondary"
             className="ms-auto"
-            onClick={onViewExpensesClick}
+            onClick={props.onViewExpensesClick}
           >
             View Expenses
           </Button>
